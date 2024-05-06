22721e493534d12fccb4014adc77191dce7a9e64
CJewon <112820744+CJewon@users.noreply.github.com>
2024-05-06 16:34:11 +0900

    study

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const product = 'MacBook'
+const model = 'Air'
+const item = product + model
+
+function handleClick() {
+      alert('곧 도착합니다!')
+}
 root.render(
-      <h1>안녕 리액트!</h1>
+      <>
+            <h1>나만의 {item} 주문하기</h1>
+            <button onClick={handleClick}>확인</button>
+      </>
       
 );
 
