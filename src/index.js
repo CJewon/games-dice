96af195c5cd4c5a97d9174801711a1c07f00fb79
CJewon <112820744+CJewon@users.noreply.github.com>
2024-05-16 10:54:33 +0900

    html + javascript = react

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-import ReactDOM from 'react-dom/client';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const product = 'MacBook'
-const model = 'Air'
-const item = product + model
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const product = "MacBook";
+const model = "Air";
+const item = product + model;
 
 function handleClick() {
-      alert('곧 도착합니다!')
+  alert("곧 도착합니다!");
 }
 root.render(
-      <>
-            <h1>나만의 {item} 주문하기</h1>
-            <button onClick={handleClick}>확인</button>
-      </>
-      
+  <>
+    <h1>나만의 {item} 주문하기</h1>
+    <button onClick={handleClick}>확인</button>
+  </>
 );
-
